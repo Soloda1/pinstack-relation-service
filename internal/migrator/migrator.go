2b96be04d0e90811a1b5093070627c686d1e9464
SolyaraDaDaYa <enderderder@gmail.com>
2025-06-16 15:44:26 +0400

    fix

--- internal/migrator/migrator.go
+++ internal/migrator/migrator.go
@@ -2,6 +2,7 @@ package migrator
 
 import (
 	"errors"
+	"github.com/golang-migrate/migrate/v4"
 	"pinstack-relation-service/internal/logger"
 
 	_ "github.com/golang-migrate/migrate/v4/database/postgres"
