9ea02b0d506a3406eb3091b5af7f928875fb8d66
SolyaraDaDaYa <enderderder@gmail.com>
2025-06-16 16:12:13 +0400

    feat: добавил типы для outbox status , добавил CHECK в схему таблицы бд

--- internal/model/outbox.go
+++ internal/model/outbox.go
@@ -5,12 +5,21 @@ import (
 	"time"
 )
 
+type OutboxStatus string
+
+const (
+	OutboxStatusNew     OutboxStatus = "new"
+	OutboxStatusPending OutboxStatus = "pending"
+	OutboxStatusSent    OutboxStatus = "sent"
+	OutboxStatusError   OutboxStatus = "error"
+)
+
 type OutboxEvent struct {
 	ID          int64           `json:"id"`
 	AggregateID int64           `json:"aggregate_id"`
 	EventType   string          `json:"event_type"`
 	Payload     json.RawMessage `json:"payload"`
-	Status      string          `json:"status"`
+	Status      OutboxStatus    `json:"status"`
 	CreatedAt   time.Time       `json:"created_at"`
 	SentAt      *time.Time      `json:"sent_at"`
 }
