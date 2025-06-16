7730cc6ec9436e9a661e1958da023a0dd764dc01
SolyaraDaDaYa <enderderder@gmail.com>
2025-06-16 19:56:58 +0400

    feat: добавил реализацию сервиса FollowService для управления подписками

--- internal/service/interface.go
+++ internal/service/interface.go
@@ -1,1 +1,10 @@
 package service
+
+import "context"
+
+type FollowService interface {
+	Follow(ctx context.Context, followerID, followeeID int64) error
+	Unfollow(ctx context.Context, followerID, followeeID int64) error
+	GetFollowers(ctx context.Context, followeeID int64) ([]int64, error)
+	GetFollowees(ctx context.Context, followerID int64) ([]int64, error)
+}
