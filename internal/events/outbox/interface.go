c1cfa5f0e9b7258c8d8c38015a5c2a39f5589787
SolyaraDaDaYa <enderderder@gmail.com>
2025-06-17 15:03:14 +0400

    feat: добавил метод AddEvent в OutboxRepository для добавления событий в outbox

--- internal/events/outbox/interface.go
+++ internal/events/outbox/interface.go
@@ -1,4 +1,10 @@
 package outbox
 
+import (
+	"context"
+	"pinstack-relation-service/internal/model"
+)
+
 type OutboxRepository interface {
+	AddEvent(ctx context.Context, outbox model.OutboxEvent) error
 }
