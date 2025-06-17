29128025dbe9f7d2bd831006d1a219e549a67322
SolyaraDaDaYa <enderderder@gmail.com>
2025-06-17 14:42:07 +0400

    feat: обновил метод Create в FollowRepository для возврата модели Follower и обработки ошибок

--- internal/repository/interface.go
+++ internal/repository/interface.go
@@ -1,10 +1,13 @@
 package repository
 
-import "context"
+import (
+	"context"
+	"pinstack-relation-service/internal/model"
+)
 
 //go:generate mockery --name=FollowRepository --output=../../mocks --outpkg=mocks --case=underscore --with-expecter
 type FollowRepository interface {
-	Create(ctx context.Context, followerID, followeeID int64) error
+	Create(ctx context.Context, followerID, followeeID int64) (model.Follower, error)
 	Delete(ctx context.Context, followerID, followeeID int64) error
 	Exists(ctx context.Context, followerID, followeeID int64) (bool, error)
 	GetFollowers(ctx context.Context, followeeID int64, limit, offset int32) ([]int64, error)
